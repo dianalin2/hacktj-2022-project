6eb39e5189497436d0525049d8a335b6aea9cd34
Diana Lin <lindiana1206@gmail.com>
2022-04-16 19:05:29 -0400

    feat: add fist detection

--- static/js/index.js
+++ static/js/index.js
@@ -12,64 +12,62 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         console.log(err);
     });
 
-
+function createFileFromUrl(path, url, callback) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function (ev) {
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                let data = new Uint8Array(request.response);
+                cv.FS_createDataFile('/', path, data, true, false, false);
+                callback();
+            } else {
+                self.printError('Failed to load ' + url + ' status: ' + request.status);
+            }
+        }
+    };
+    request.send();
+};
 
 function onOpenCvLoad() {
-    // cv.onRuntimeInitialized = () => {
-    // const cap = new cv.VideoCapture(video);
-    // const src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    // const gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-    console.log('here')
+    cv.onRuntimeInitialized = () => {
+        const cap = new cv.VideoCapture(video);
+        const src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+        const gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
 
-    fetch('/static/hc/hand.xml').then(res => { console.log(res); return res.blob() }).then(xml => {
-        console.log("xml")
-        console.log(xml);
-        // const data = new Uint8Array(xml);
-        // cv.FS_createDataFile('/', 'hand.xml', data, true, false, false);
+        createFileFromUrl('hand.xml', '/static/hc/hand.xml', () => {
 
-        // const handCascade = new cv.CascadeClassifier();
-        // const hands = new cv.RectVector();
+            const handCascade = new cv.CascadeClassifier();
+            const hands = new cv.RectVector();
 
-        // // handCascade.load('hand.xml');
+            handCascade.load('hand.xml');
 
-        // function processVideo() {
-        //     let begin = Date.now();
-        //     cap.read(src);
-        //     cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+            function processVideo() {
+                let begin = Date.now();
+                cap.read(src);
+                cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
 
-        //     let msize = new cv.Size(0, 0);
+                let msize = new cv.Size(0, 0);
 
-        //     // handCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
-        //     // console.log(hands);
+                handCascade.detectMultiScale(gray, hands, 1.1, 3, 0, msize, msize);
+                console.log(hands);
 
-        //     // for (let i = 0; i < faces.size(); ++i) {
-        //     //     let roiGray = gray.roi(faces.get(i));
-        //     //     let roiSrc = src.roi(faces.get(i));
-        //     //     let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-        //     //     let point2 = new cv.Point(faces.get(i).x + faces.get(i).width,
-        //     //         faces.get(i).y + faces.get(i).height);
-        //     //     cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
-        //     //     // detect eyes in face ROI
-        //     //     eyeCascade.detectMultiScale(roiGray, eyes);
-        //     //     for (let j = 0; j < eyes.size(); ++j) {
-        //     //         let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
-        //     //         let point2 = new cv.Point(eyes.get(j).x + eyes.get(j).width,
-        //     //             eyes.get(j).y + eyes.get(j).height);
-        //     //         cv.rectangle(roiSrc, point1, point2, [0, 0, 255, 255]);
-        //     //     }
-        //     //     roiGray.delete(); roiSrc.delete();
-        //     // }
+                for (let i = 0; i < hands.size(); ++i) {
+                    let point1 = new cv.Point(hands.get(i).x, hands.get(i).y);
+                    let point2 = new cv.Point(hands.get(i).x + hands.get(i).width,
+                        hands.get(i).y + hands.get(i).height);
+                    cv.rectangle(src, point1, point2, [255, 0, 0, 255], 5, cv.LINE_8, 0);
+                }
 
-        //     cv.imshow(output1.id, gray);
-        //     // schedule next one.
-        //     let delay = 1000 / FPS - (Date.now() - begin);
-        //     setTimeout(processVideo, delay);
-        // }
-        // setTimeout(processVideo, 0);
-    }).catch(err => {
-        console.log(err);
-    });
-    // }
+                cv.imshow(output1.id, src);
+
+                let delay = 1000 / FPS - (Date.now() - begin);
+                setTimeout(processVideo, delay);
+            }
+            setTimeout(processVideo, 0);
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 }
-
-console.log('here')
