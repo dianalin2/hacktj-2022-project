a01cfe77193592c53a4ca9f3fbc52a3c3934d569
Diana Lin <lindiana1206@gmail.com>
2022-04-16 20:28:47 -0400

    refactor: move hand detection to index

--- static/js/hand-detection.js
+++ static/js/hand-detection.js
@@ -3,6 +3,8 @@ const output1 = document.getElementById("output-1");
 
 const FPS = 30;
 
+let currentCountdown = null;
+
 navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(stream => {
         video.srcObject = stream;
@@ -51,7 +53,6 @@ function onOpenCvLoad() {
                 let msize = new cv.Size(0, 0);
 
                 handCascade.detectMultiScale(gray, hands, 1.1, 3, 0, msize, msize);
-                console.log(hands);
 
                 for (let i = 0; i < hands.size(); ++i) {
                     let point1 = new cv.Point(hands.get(i).x, hands.get(i).y);
@@ -60,14 +61,22 @@ function onOpenCvLoad() {
                     cv.rectangle(src, point1, point2, [255, 0, 0, 255], 5, cv.LINE_8, 0);
                 }
 
+                if (hands.size() > 0 && currentCountdown == null) {
+                    setTimeout(setCountdown, 1000);
+                }
+
                 cv.imshow(output1.id, src);
 
                 let delay = 1000 / FPS - (Date.now() - begin);
                 setTimeout(processVideo, delay);
             }
             setTimeout(processVideo, 0);
-        }).catch(err => {
-            console.log(err);
         });
     }
+}
+
+function setCountdown(time) {
+    console.log('Picture taken!');
+
+    currentCountdown = null;
 }
