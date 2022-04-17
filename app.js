afb0160f861484e8d57432c447c9ec142dd90375
Diana Lin <lindiana1206@gmail.com>
2022-04-17 06:49:18 -0400

    fix: remove /test endpoint

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express();
 app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
-app.get('/test', (req, res) => res.sendFile(__dirname + '/views/test.html'));
 
 app.listen(3000, '0.0.0.0', () => {
     console.log('Server is running on port 3000');
